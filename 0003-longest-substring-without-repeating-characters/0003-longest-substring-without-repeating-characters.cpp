ebbe978ada8e31ca89eff9cf580d132bdf9ae29c
Mudit Golchha <muditgolchha35@gmail.com>
2026-08-22 14:22:02 +0530

    Time: 47 ms (29.85%), Space: 19.4 MB (22.89%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,11 +5,9 @@ public:
         int left = 0;
         int maxlenght = 0;
         for(int i = 0;i<s.size();i++){
-            char c = s[i];
-            mpp[c]++;
-            while(mpp[c]>1){
-                char leftchar = s[left];
-                mpp[leftchar]--;
+            mpp[s[i]]++;
+            while(mpp[s[i]]>1){
+                mpp[s[left]]--;
                 left++;
             }
             maxlenght = max(maxlenght,i-left+1);
