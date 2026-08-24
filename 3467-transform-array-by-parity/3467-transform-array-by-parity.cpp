fa565dbfeed3980753651c9c52d770be0ef38b44
Mudit Golchha <muditgolchha35@gmail.com>
2026-08-24 09:27:04 +0530

    Time: 0 ms (100%), Space: 28.3 MB (62.19%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.cpp
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     vector<int> transformArray(vector<int>& nums) {
+        int count0 = 0;
+        int count1 = 0;
         for(int i = 0;i<nums.size();i++){
             if(nums[i] % 2 == 0){
                 nums[i]=0;
+                count0++;
             }
             else{
                 nums[i]=1;
+                count1++;
             }
         }
-        sort(nums.begin(),nums.end());
+        for(int i = 0;i<count0;i++){
+            nums[i]=0;
+        }
+        for(int i = count0;i<nums.size();i++){
+            nums[i]=1;
+        }
         return nums;
         
     }
