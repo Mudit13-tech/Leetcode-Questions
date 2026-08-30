66516aa09cf7cf9a689c41b3f488e9c72dd2b6e7
Mudit Golchha <muditgolchha35@gmail.com>
2026-08-30 14:50:16 +0530

    Time: 3 ms (41.33%), Space: 12.7 MB (62.31%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int>st1;
-        stack<int>st2;
-        stack<int> q;
+        stack<int> st;
+        unordered_map<int,int> mpp;
         vector<int> ans;
-        for(int i = nums1.size()-1 ; i>=0 ; i--){
-            st1.push(nums1[i]);
-        }
-        for(int i = nums2.size()-1 ; i>=0 ; i--){
-            st2.push(nums2[i]);
+        for (int i = nums2.size()-1 ; i>=0 ; i--){
+            while(!st.empty() && nums2[i]>st.top()){
+                st.pop();
+            }
+            if(!st.empty() && nums2[i]<st.top()){
+                mpp[nums2[i]]=st.top();
+            }
+            else {
+                mpp[nums2[i]]= -1;
+            }
+            st.push(nums2[i]);
         }
         for(int i = 0;i<nums1.size();i++){
-            while(!st2.empty() && st1.top() != st2.top()){
-                q.push(st2.top());
-                st2.pop();
-            }
-            q.push(st2.top());
-            st2.pop();
-
-            while(!st2.empty() && st1.top() > st2.top()){
-                q.push(st2.top());
-                st2.pop();
-            }
-            if(!st2.empty() && st1.top() < st2.top()){
-                ans.push_back(st2.top());
-            }
-            else{
-                ans.push_back(-1);
-            }
-            while(!q.empty()){
-                st2.push(q.top());
-                q.pop();
-            }
-            st1.pop();
+            ans.push_back(mpp[nums1[i]]);
         }
         return ans;
-        
     }
 };
